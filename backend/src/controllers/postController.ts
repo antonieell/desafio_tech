e983a098dffd3c227bc89f80782c554e1f41d50a
Antoniel Magalhães <antoniel2210@gmail.com>
2020-10-22 00:36:53 -0300

    feat: add comments in a post

--- backend/src/controllers/postController.ts
+++ backend/src/controllers/postController.ts
@@ -1,6 +1,5 @@
-import {Request, Response} from 'express'
-import Post from '../models/post'
-
+import { Request, Response } from "express";
+import Post from "../models/post";
 
 /* GET /posts 
 Retorna a lista de posts */
@@ -10,7 +9,7 @@ export const getPosts = async (req: Request, res: Response) => {
     return res.status(200).send(allPosts);
   } catch (error) {
     console.log(error);
-    return res.status(500)
+    return res.status(500);
   }
 };
 
@@ -23,10 +22,9 @@ export const getPostsById = async (req: Request, res: Response) => {
     return res.status(200).send(getPostById);
   } catch (error) {
     console.log(error);
-    return res.send({err: "Post não encontrado"}).status(404)
+    return res.send({ err: "Post não encontrado" }).status(404);
   }
 };
-
 
 /* GET /posts/:id/comments Retorna a lista de comentários para um determinado post, 
 caso não exista deverá retornar 404 Not Found */
@@ -37,23 +35,38 @@ export const getCommentsById = async (req: Request, res: Response) => {
     return res.status(200).send(getPostById);
   } catch (error) {
     console.log(error);
-    return res.send({err: "Post não encontrado"}).status(404)
+    return res.send({ err: "Post não encontrado" }).status(404);
   }
 };
 
 /* POST /posts 
 Faz a inclusão de um novo post */
-export const createPost= async (req: Request, res: Response) => {
-const {content} = req.body
+export const createPost = async (req: Request, res: Response) => {
+  const { content } = req.body;
   try {
-    const post = await Post.create({content})
-    return res.json(post)
+    const post = await Post.create({ content });
+    return res.json(post);
   } catch (error) {
     console.log(error);
-    return res.json({err: "Não foi possível criar o post"}).status(500)
+    return res.json({ err: "Não foi possível criar o post" }).status(500);
   }
 };
 
 /* POST /posts/:id/comments 
 Faz a inclusão de um novo comentário para um determinado post */
 
+export const createComment = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { content } = req.body;
+  try {
+    const post = await Post.findById(id);
+    if(post){
+      post.comments?.push(content)
+      await post.save()
+    }
+    return res.json(post);
+  } catch (error) {
+    console.log(error);
+    return res.json({ err: "Não foi possível encontrar o post" }).status(500);
+  }
+};
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -5,5 +5,6 @@ const router = Router()
 router.get('/posts', postController.getPosts)
 router.get('/posts/:id', postController.getPostsById)
 router.post('/posts', postController.createPost)
+router.post('/posts/:id/comments', postController.createComment)
 
 export default router
