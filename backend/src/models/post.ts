479dcefb43319f3320aff955711557ba91686044
Antoniel Magalhães <antoniel2210@gmail.com>
2020-10-22 00:26:26 -0300

    add: strong type in model

--- backend/src/models/post.ts
+++ backend/src/models/post.ts
@@ -1,10 +1,16 @@
+import {Document, Schema} from 'mongoose'
 import mongoose from "../database/index";
 
-const PostSchema = new mongoose.Schema({
+export interface Post extends Document {
+  content: string;
+  comments?: string[];
+}
+
+const PostSchema : Schema = new mongoose.Schema({
   content: { type: String, required: true },
   comments: { type: [String] },
 });
 
-const Post = mongoose.model("Post", PostSchema);
+const Post = mongoose.model<Post>("Post", PostSchema);
 
 export default Post;
