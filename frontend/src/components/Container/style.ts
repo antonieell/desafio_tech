77ae6507921427cf0f0c10bc62d6cecb4eea3f05
Antoniel Magalhães <antoniel2210@gmail.com>
2020-10-24 15:59:12 -0300

    refactor: change className

--- frontend/src/components/Container/style.ts
+++ frontend/src/components/Container/style.ts
@@ -8,7 +8,7 @@ export const Container = styled.div`
   box-shadow: 0 7px 30px -10px rgba(150, 170, 180, 0.5);
   padding: 2rem;
   margin: 0 2rem;
-  &.title{
+  &.header{
     margin-bottom: 2.4rem;
   }
   @media (min-width: 1180px) {
